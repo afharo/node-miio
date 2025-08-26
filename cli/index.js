a99cb4f83a0eeccddd7f17d03f2370f250bdd3b6
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2025-08-27 00:34:19 +0200

    Upgrade yargs

--- cli/index.js
+++ cli/index.js
@@ -2,7 +2,11 @@
 'use strict';
 
 const path = require('path');
-require('yargs')
+const yargs = require('yargs');
+// eslint-disable-next-line node/no-missing-require
+const { hideBin } = require('yargs/helpers');
+
+yargs(hideBin(process.argv))
 	.commandDir(path.join(__dirname, 'commands'))
 	.recommendCommands()
 	.demandCommand()
